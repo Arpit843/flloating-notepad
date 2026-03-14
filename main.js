5f830fc8d7ea629a0f69fc425a1a4f234b721c41
Arpit Shakkerwal <112974894+Arpit843@users.noreply.github.com>
2026-03-14 17:22:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, screen } = require('electron');
 const path = require('path');
-
-function createWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 300,
-    height: 400,
+ 
+let mainWindow;
+ 
+function createWindow() {
+  const { width } = screen.getPrimaryDisplay().workAreaSize;
+ 
+  mainWindow = new BrowserWindow({
+    width: 340,
+    height: 480,
+    x: width - 360,
+    y: 40,
     transparent: true,
     frame: false,
     resizable: true,
+    minWidth: 240,
+    minHeight: 200,
+    alwaysOnTop: true,
+    skipTaskbar: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   });
-
+ 
   mainWindow.loadFile('index.html');
 }
+ 
 app.whenReady().then(createWindow);
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
+app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
+ 
+ipcMain.on('close-window', () => mainWindow.close());
+ipcMain.on('minimize-window', () => mainWindow.minimize());
+ipcMain.on('toggle-always-on-top', (event) => {
+  const current = mainWindow.isAlwaysOnTop();
+  mainWindow.setAlwaysOnTop(!current);
+  event.reply('always-on-top-changed', !current);
+});
+ipcMain.on('get-always-on-top', (event) => {
+  event.reply('always-on-top-changed', mainWindow.isAlwaysOnTop());
+});
